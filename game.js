52714c37732b0887fa89d4bb93c587f97a2400a6
spaasie <68701772+spaasie@users.noreply.github.com>
2022-03-02 09:44:53 +0200

    create 'playRound()' function to evaluate win/loser and return appropriate message

--- game.js
+++ game.js
@@ -5,3 +5,27 @@ function computerPlay() {
   return shapes[randomInt];
 }
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "That is a draw, try again...";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats Paper";
+  } else {
+    return "Error! self-destruct sequence initiated ...";
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
